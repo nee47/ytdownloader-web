dc8ea22238b831285be44bf8991d9931c4cb100e
joseph <joseph_vv@hotmail.com>
2024-05-10 22:05:08 -0300

    still implemintg tests

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,26 +1,31 @@
 const path = require("path");
 
-function searchVid(req, res){
-    var exec = require("child_process").execFile;
+function searchVid(req, res) {
+  var exec = require("child_process").execFile;
 
-    const youtubeUrlRegex = /^(https?:\/\/)?(www\.)?youtu\.?be(\.com)?\/.+$/;
-    if(!youtubeUrlRegex.test(req.body.url))
-        res.send(400)
-    else
-        exec(
-          `${path.resolve(__dirname, '..')}/resources/yt-dlp.exe`,
-          ["-e", "--get-thumbnail", req.body.url],
-          function (err, data) {
-            if(err){
-                const estoError = "esto es error "+err.toString()
-                console.log(estoError);
-            }
-            const esto = "ESTO ES DATA : "+data.toString()
-            console.log(esto);
-            res.json({ message: "UPDATE" });
-          }
-        ); 
+  const youtubeUrlRegex =
+    /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/;
+
+  if (!youtubeUrlRegex.test(req.body.url)) {
+    console.log("NOT VALID  LINK");
+    res.send(400);
+  } else {
+    console.log("VALID VIDEO LINK");
+    exec(
+      `${path.resolve(__dirname, "..")}/resources/yt-dlp.exe`,
+      ["-e", "--get-thumbnail", req.body.url],
+      function (err, data) {
+        if (err) {
+          const estoError = "esto es error " + err.toString();
+          console.log(estoError);
+        }
+        const esto = "ESTO ES DATA : " + data.toString();
+        console.log(esto);
+        res.json({ message: "UPDATE" });
+      }
+    );
   }
+}
 
 //export default searchVid;
-module.exports = {searchVid}
+module.exports = { searchVid };
--- server/tests/searchController.test.js
+++ server/tests/searchController.test.js
@@ -1,7 +1,30 @@
-const {searchVid} = require("./../controllers/searchController")
+const { searchVid } = require("./../controllers/searchController");
+const childProcess = require("child_process");
 
-it("it should  return the title for a valid existing video", ()=>{
-    const req = null;
-    const res = null;
-    searchVid(req, res);
-})
+jest.mock("child_process");
+
+it("it should  return the title for a valid existing video", () => {
+  const req = {
+    body: {
+      url: "https://www.youtube.com/watch?v=giywdKF4SXY",
+    },
+  };
+  const res = null;
+
+  const mockExec = jest.spyOn(childProcess, "exec");
+  mockExec.mockImplementation((command, callback) => {
+    console.log("MOCKED");
+    console.log(command);
+    // Mock the behavior of child_process.exec
+    // For example, you can simulate a successful execution:
+    callback(null, "Mocked output", "Mocked error");
+  });
+
+  searchVid(req, res);
+
+  expect(mockExec).toHaveBeenCalledWith(
+    // Add your expected command here
+    "your-command",
+    expect.any(() => console.log("printed"))
+  );
+});
